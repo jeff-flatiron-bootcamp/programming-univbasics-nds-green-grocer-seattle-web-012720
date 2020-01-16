5fed430bb9edab5b67ec455d16649137b5a8ebde
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 03:32:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,7 +59,7 @@ def apply_coupons(cart, coupons)
     
     cart_index = 0
     while(cart[cart_index]) do
-      cart[cart_index]
+      cart[cart_index}]
     end
   end
   puts "END APPLY COUPONS"
